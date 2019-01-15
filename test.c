cb358ffaa9bad991765ae75674423c634488dc5d
Keijiro Takahashi <keijiro@gmail.com>
2019-01-15 18:39:18 +0900

    scan all frames

--- test.c
+++ test.c
@@ -9,28 +9,32 @@ void hap_callback(HapDecodeWorkFunction function, void *p, unsigned int count, v
 
 int main()
 {
-    FILE* file = fopen("hap.mov", "rb");
+    FILE *file = fopen("hap.mov", "rb");
 
     MP4D_demux_t demux;
     MP4D__open(&demux, file);
 
-    unsigned int length, timestamp, duration;
-    MP4D_file_offset_t offs = MP4D__frame_offset(&demux, 0, 0, &length, &timestamp, &duration);
+    void *in_buffer = malloc(4 * 1024 * 1024);
+    void *out_buffer = malloc(4 * 1024 * 1024);
 
-    void* in_buffer = malloc(length);
-    void* out_buffer = malloc(1024 * 1024);
+    for (unsigned int i = 0; i < demux.track[0].sample_count; i++)
+    {
+        unsigned int in_size, timestamp, duration;
+        MP4D_file_offset_t in_offs = MP4D__frame_offset(&demux, 0, i, &in_size, &timestamp, &duration);
 
-    fseek(file, offs, SEEK_SET);
-    fread(in_buffer, length, 1, file);
+        fseek(file, in_offs, SEEK_SET);
+        fread(in_buffer, in_size, 1, file);
 
-    unsigned long used;
-    unsigned int format;
-    unsigned int res = HapDecode(in_buffer, length, 0, hap_callback, NULL, out_buffer, 1024 * 1024, &used, &format);
+        unsigned long used;
+        unsigned int format;
+        unsigned int res = HapDecode(in_buffer, in_size, 0, hap_callback, NULL, out_buffer, 1024 * 1024, &used, &format);
 
-    printf("res:%d format:%x size:%ld\n", res, format, used);
+        printf("frm:%d res:%d format:%x size:%ld\n", i, res, format, used);
+    }
 
     free(in_buffer);
     free(out_buffer);
     fclose(file);
+
     return 0;
 }
