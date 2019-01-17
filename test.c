e2627017f6de01e639f0f63491f8b5235f42c528
Keijiro Takahashi <keijiro@gmail.com>
2019-01-17 15:05:45 +0900

    refactoring

--- test.c
+++ test.c
@@ -1,73 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define MINIMP4_IMPLEMENTATION
 #include "mp4demux.h"
-
 #include "hap.h"
 
-void hap_callback(HapDecodeWorkFunction function, void *p, unsigned int count, void *info)
+static void hap_callback
+    (HapDecodeWorkFunction function, void *p, unsigned int count, void *info)
 {
 }
 
-static void print_mp4_info(const MP4D_demux_t * mp4_demux)
+static double calc_time(uint32_t hi, uint32_t lo, uint32_t scale)
 {
-    unsigned i;
+    return (double)(((uint64_t)hi << 32) + lo) / scale;
+}
 
-    printf("\nMP4 FILE: %d tracks found. Movie time %.2f sec\n", mp4_demux->track_count, (4294967296.0*mp4_demux->duration_hi + mp4_demux->duration_lo) / mp4_demux->timescale);
-    printf("\nNo|type|lng| duration           | bitrate| %-23s| Object type","Stream type");
-    for (i = 0; i < mp4_demux->track_count; i++)
-    {
-        MP4D_track_t * tr = mp4_demux->track + i;
-
-        printf("\n%2d|%c%c%c%c|%c%c%c|%7.2f s %6d frm| %7d|", i,
-            (tr->handler_type>>24), (tr->handler_type>>16), (tr->handler_type>>8), (tr->handler_type>>0),
-            tr->language[0],tr->language[1],tr->language[2],
-            (65536.0* 65536.0*tr->duration_hi + tr->duration_lo) / tr->timescale,
-            tr->sample_count,
-            tr->avg_bitrate_bps
-            );
-
-        printf(" %-23s|", MP4D__stream_type_to_ascii(tr->stream_type));
-        printf(" %-23s", MP4D__object_type_to_ascii(tr->object_type_indication));
-
-        if (tr->handler_type == MP4_HANDLER_TYPE_SOUN)
-        {
-            printf("  -  %d ch %d hz", tr->SampleDescription.audio.channelcount, tr->SampleDescription.audio.samplerate_hz);
-        }
-        else if (tr->handler_type == MP4_HANDLER_TYPE_VIDE)
-        {
-            printf("  -  %dx%d", tr->SampleDescription.video.width, tr->SampleDescription.video.height);
-        }
-    }
-    printf("\n");
+static const char *str_fourcc(uint32_t code)
+{
+    static char str[5];
+    str[0] = code >> 24;
+    str[1] = code >> 16;
+    str[2] = code >> 8;
+    str[3] = code;
+    return str;
 }
 
 int main()
 {
-    FILE *file = fopen("hap.mov", "rb");
+    FILE *file = fopen("test.mov", "rb");
 
     MP4D_demux_t demux;
     MP4D__open(&demux, file);
-    print_mp4_info(&demux);
 
     void *in_buffer = malloc(4 * 1024 * 1024);
     void *out_buffer = malloc(4 * 1024 * 1024);
 
-    unsigned int frame_count = demux.track[0].sample_count;
-    for (unsigned int i = 0; i < frame_count; i += frame_count / 10)
+    for (int track_index = 0; track_index < demux.track_count; track_index++)
     {
-        unsigned int in_size, timestamp, duration;
-        mp4d_size_t in_offs = MP4D__frame_offset(&demux, 0, i, &in_size, &timestamp, &duration);
+        const MP4D_track_t *track = &demux.track[track_index];
 
-        fseek(file, in_offs, SEEK_SET);
-        fread(in_buffer, in_size, 1, file);
+        printf("Track %d '%s' %.2fs %d frames",
+               track_index,
+               str_fourcc(track->handler_type),
+               calc_time(track->duration_hi,
+                         track->duration_lo,
+                         track->timescale),
+               track->sample_count);
 
-        unsigned long used;
-        unsigned int format;
-        unsigned int res = HapDecode(in_buffer, in_size, 0, hap_callback, NULL, out_buffer, 1024 * 1024, &used, &format);
+        if (track->handler_type != MP4_HANDLER_TYPE_VIDE)
+        {
+            puts("");
+            continue;
+        }
 
-        printf("frm:%d res:%d format:%x size:%ld\n", i, res, format, used);
+        printf(" %dx%d\n",
+               track->SampleDescription.video.width,
+               track->SampleDescription.video.height);
+
+        for (unsigned int sample_index = 0;
+             sample_index < track->sample_count;
+             sample_index += track->sample_count / 10)
+        {
+            unsigned int in_size, timestamp, duration;
+            mp4d_size_t in_offs = MP4D__frame_offset
+                (&demux, 0, sample_index, &in_size, &timestamp, &duration);
+
+            fseek(file, in_offs, SEEK_SET);
+            fread(in_buffer, in_size, 1, file);
+
+            unsigned long used;
+            unsigned int format;
+            unsigned int res = HapDecode(in_buffer, in_size, 0,
+                                         hap_callback, NULL,
+                                         out_buffer, 1024 * 1024,
+                                         &used, &format);
+
+            printf("Frame %d Result:%d Format:%x Size:%ld\n",
+                   sample_index, res, format, used);
+        }
     }
 
     free(in_buffer);
